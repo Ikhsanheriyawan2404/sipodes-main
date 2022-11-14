a9499e96d89cd1f0618379790c9be881cf855705
hariyadiwijaya1 <hariyadiwijaya1@gmail.com>
2022-11-14 11:47:15 +0700

    Fix transaction data

--- app/Http/Controllers/API/V1/WisataController.php
+++ app/Http/Controllers/API/V1/WisataController.php
@@ -5,13 +5,19 @@ namespace App\Http\Controllers\API\V1;
 use App\Models\{Wisata, Desa};
 use App\Http\Resources\ApiResource;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\WisataRequest;
 use Illuminate\Support\Facades\Validator;
 
 class WisataController extends Controller
 {
     public function index()
     {
-        return response()->json(Wisata::with('desa')->get());
+        return response()->json(new ApiResource(200, true, 'Data Wisata', Wisata::with('desa')->get()));
+    }
+
+    public function show($codeDesa, $wisataId)
+    {
+        return response()->json(new ApiResource(200, true, 'Detail Wisata', Wisata::where('code_desa', $codeDesa)->where('wisata_id', $desaId)->get()), 200);
     }
 
     public function store()
@@ -44,16 +50,17 @@ class WisataController extends Controller
                 'meta_description' => request('meta_description'),
                 'meta_keyword' => request('meta_keyword'),
             ]);
+            
             return response()->json(new ApiResource(201, true, 'Data wisata pusat berhasil ditambahkan'), 201);
         } catch (\Exception $e) {
             return response()->json(new ApiResource(400, false, $e->getMessage()), 400);
         }
     }
 
-    public function update($id)
+    public function update($codeDesa, $wisataId)
     {
         $validator = Validator::make(request()->all(), [
-            'code_desa' => 'required',
+            'wisata_id' => 'required',
             'name' => 'required|max:255',
             'description' => 'required',
             'location' => 'required|max:255',
@@ -62,14 +69,17 @@ class WisataController extends Controller
             'price' => 'required|max:255',
         ]);
 
-        if ($validator->fails()) {
+        if (!$validator->fails()) {
             return response()->json(new ApiResource(400, false, $validator->errors), 400);
         }
 
-        $wisata = Wisata::find($id);
+        $wisata = Wisata::where('code_desa', $codeDesa)->where('wisata_id', $wisataId)->get();
+        if ($wisata) {
+            return response()->json('Data tidak ditemukan', 404);
+        }
         try {
             $wisata->update([
-                'code_desa' => request('code_desa'),
+                'wisata_id' => request('wisata_id'),
                 'name' => request('name'),
                 'description' => request('description'),
                 'location' => request('location'),
@@ -85,12 +95,15 @@ class WisataController extends Controller
         }
     }
 
-    public function destroy($id)
+    public function destroy($codeDesa, $wisataId)
     {
+        $wisata = Wisata::where('code_desa', $codeDesa)->where('wisata_id', $wisataId)->first();
+        if (!$wisata) {
+            return response()->json('Berhasil tidak ditemukan', 404);
+        }
         try {
-            $wisata = Wisata::where('wisata_id', $id)->first();
-            $wisata->where('code_desa', $wisata->code_desa)->delete();
-            return response()->json('Data wisata pusat berhasil dihapus', 200);
+            $wisata->delete();
+            return response()->json('Berhasil hapus data dari pusat', 200);
         } catch (\Exception $e) {
             return response()->json($e->getMessage(), 400);
         }
--- routes/api.php
+++ routes/api.php
@@ -5,25 +5,16 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\V1\WisataController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::middleware('auth:sanctum')->group(function () {
 
 });
 
 
 Route::prefix('v1')->group(function () {
-    // Route::delete('wisata/{id}', [WisataController::class, 'destroy']);
-    Route::apiResource('wisata', WisataController::class);
+    Route::apiResource('wisata', WisataController::class)->only('index', 'store');
+    Route::get('wisata/{codeDesa}/{wisataId}', [WisataController::class, 'show']);
+    Route::put('wisata/{codeDesa}/{wisataId}', [WisataController::class, 'update']);
+    Route::delete('wisata/{codeDesa}/{wisataId}', [WisataController::class, 'destroy']);
 
     Route::get('desa', [DesaController::class, 'index']);
     Route::post('desa', [DesaController::class, 'store']);
